47fb7d4ed9cecdf8827ba1f6aac8663b85e56d44
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2021-09-26 01:18:00 -0300

    creating modal and http calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
+
+//For accessibility
+Modal.setAppElement('#root')
+
+
 export function App() {
-  return (
-    <>
-      <Header />
-      <Dashboard />
-      <GlobalStyle />
-    </>
-  );
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
+
+    return (
+        <>
+
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+        <Dashboard />
+
+        <NewTransactionModal 
+            isOpen={isNewTransactionModalOpen}
+            onRequestClose={handleCloseNewTransactionModal}
+        />
+
+        <GlobalStyle />
+
+        </>
+    );
 }
 
 
